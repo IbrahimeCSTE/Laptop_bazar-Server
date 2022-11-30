9e30d4853311a7d9100903771fe775c2ac3efbc2
cste14 <emonnstu14@gmail.com>
2022-11-30 16:59:54 +0600

    db create and test

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 //port
 const PORT = process.env.PORT || 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tfzr2.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const userCollection = client.db("IMS").collection("user");
+const categoryCollection = client.db("IMS").collection("category");
+const productCollection = client.db("IMS").collection("product");
+const orderCollection = client.db("IMS").collection("orders");
+const paymentsCollection = client.db("IMS").collection("payment");
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
